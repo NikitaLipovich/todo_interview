db48e73ba3f84bb10cd9dc16060cf0bc896f2fa6
Nikita Lipovich <nick.lypovic@bloomx.ag>
2025-07-20 13:28:11 +0300

    Configure OpenAPI documentation with Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors();
+  
+  // Setup OpenAPI documentation
+  const config = new DocumentBuilder()
+    .setTitle('TODO API')
+    .setDescription('The TODO API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
